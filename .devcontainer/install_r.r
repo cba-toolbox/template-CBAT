c2f79f0327631c3be71e3b78b00e5dbec6b5014a
Yoshihiko Kunisato <ykunisato@psy.senshu-u.ac.jp>
2025-09-15 11:58:32 +0900

    init

---
+++ .devcontainer/install_r.r
@@ -0,0 +1,7 @@
+# 追加したいパッケージがあれば追加する。
+# install R packages from CRAN
+# install.packages(c("languageserver","vscDebugger"))
+
+
+# install from GitHub
+remotes::install_github(c("ykunisato/psyinfr"), dependencies = TRUE)
